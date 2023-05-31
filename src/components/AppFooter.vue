bbaca73fd2b4ca52209557c70f85687c9322be97
RaghidJ <raghid.jomaa@gmail.com>
2023-05-31 15:46:11 +0200

    Text_Change_Footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,14 +3,14 @@
         <div class="v-app-footer__line" >
           <div class="footer-white">F1:<span class="red">Not Available</span></div>
           <div class="footer-white">LMB:<span class="red">CLICK</span></div>
-          <div class="footer-white">F1:<span class="red">Not Available</span></div>
-          <div class="footer-white">F1:<span class="red">Not Available</span></div>
+          <div class="footer-white">ESC:<span class="red">Not Available</span></div>
+          <div class="footer-white">ENTER:<span class="red">Not Available</span></div>
         </div>
         <div class="v-app-footer__line" >
-            <div class="footer-white">F1:<span class="red">Not Available</span></div>
+            <div class="footer-white">A:<span class="red">Not Available</span></div>
             <div class="footer-white">RMB:<span class="red">INSPECT</span></div>
-            <div class="footer-white">F1:<span class="red">Not Available</span></div>
-            <div class="footer-white">F1:<span class="red">Not Available</span></div>
+            <div class="footer-white">W:<span class="red">Not Available</span></div>
+            <div class="footer-white">D:<span class="red">Not Available</span></div>
         </div>
     </section>
 </template>
