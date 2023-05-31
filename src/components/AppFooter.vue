3d888f489cf5e6f6cc9825ce4af9b703e0d14208
RaghidJ <raghid.jomaa@gmail.com>
2023-05-31 15:40:58 +0200

    Footer_Changes

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,22 +1,19 @@
 <template>
     <section class="v-app-footer" >
         <div class="v-app-footer__line" >
-          <div>F1: Not Available</div>
-          <div>F1: Not Available</div>
-          <div>F1: Not Available</div>
-          <div>F1: Not Available</div>
+          <div class="footer-white">F1:<span class="red">Not Available</span></div>
+          <div class="footer-white">LMB:<span class="red">CLICK</span></div>
+          <div class="footer-white">F1:<span class="red">Not Available</span></div>
+          <div class="footer-white">F1:<span class="red">Not Available</span></div>
         </div>
         <div class="v-app-footer__line" >
-            <div>F1: Not Available</div>
-            <div>F1: Not Available</div>
-            <div>F1: Not Available</div>
-            <div>F1: Not Available</div>
+            <div class="footer-white">F1:<span class="red">Not Available</span></div>
+            <div class="footer-white">RMB:<span class="red">INSPECT</span></div>
+            <div class="footer-white">F1:<span class="red">Not Available</span></div>
+            <div class="footer-white">F1:<span class="red">Not Available</span></div>
         </div>
     </section>
 </template>
-
-
-
 
 
 <script lang="ts" setup>
